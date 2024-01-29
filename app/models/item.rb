d55ee2997d657b0b7af2c998b9d7366ace8fd868
supenser <fenders390@gmail.com>
2024-01-29 09:40:57 +0900

    バリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,9 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
+  validates :image, :title, :title_text, :fish_fresh_id, :fish_recipe_id, :price, presence: true
+  validates :fish_fresh_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :fish_recipe_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 50, less_than_or_equal_to: 999_999 }
+
 end
